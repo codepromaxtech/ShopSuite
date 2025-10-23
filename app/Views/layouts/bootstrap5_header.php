88f5842fe099a49d2f94c666cbb42f2b3814e645
ShopSuite Deploy <deploy@shopsuite.local>
2025-10-24 00:45:05 +0600

    Add jQuery UI and nominatim autocomplete libraries

--- app/Views/layouts/bootstrap5_header.php
+++ app/Views/layouts/bootstrap5_header.php
@@ -44,11 +44,20 @@ $config = config('ShopSuite')->settings ?? [];
     <!-- SweetAlert2 CSS (Bootstrap 5 compatible) -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     
+    <!-- jQuery UI CSS -->
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.min.css">
+    
     <!-- jQuery (required for table_support) -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     
     <!-- Moment.js (for date picker) -->
     <script src="https://cdn.jsdelivr.net/npm/moment@2.29.4/moment.min.js"></script>
+    
+    <!-- jQuery UI JS -->
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
+    
+    <!-- Nominatim Autocomplete -->
+    <script src="<?= base_url('js/nominatim.autocomplete.js') ?>" type="text/javascript"></script>
     
     <!-- Bootstrap 5 JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
